c62e0ff17e1336f163f7fd2176443f69b725a40b
Talevaishnavi <vaishnavitale93@gmail.com>
2026-06-04 11:38:46 +0530

    Added the class excericse

---
+++ metroride/metroride/src/main/java/com/metroride/metroride/MetrorideApplication.java
@@ -0,0 +1,13 @@
+package com.metroride.metroride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrorideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrorideApplication.class, args);
+	}
+
+}
---
+++ metroride/metroride/src/test/java/com/metroride/metroride/MetrorideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metroride.metroride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrorideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
